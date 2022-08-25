f7fabacb18689bc038fead37f79f9a56b64b567c
M V K <krishna.mugala9@gmail.com>
2022-08-25 16:47:24 +0530

    added fields in the class savings account

--- src/main/java/com/niit/jdp/SavingsAccount.java
+++ src/main/java/com/niit/jdp/SavingsAccount.java
@@ -9,5 +9,15 @@
 package com.niit.jdp;
 
 public class SavingsAccount {
+    private String accountHoldersName;
+    private long accountNumber;
+    private String accountOpeningDate;
+    private String accountStatus;
+    private double availableBalance;
+    private double totalBalance;
+    private boolean isMobileBankingEnabled;
+    private float intrestRate;
+    private String modeOfOperation;
+
 
 }
